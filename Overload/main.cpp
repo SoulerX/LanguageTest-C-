caf14abef0403ada5b1934913e8f4f1645136707
许志伟 <xuzhiwei3@corp.netease.com>
2024-11-21 23:27:23 +0800

    < > <<运算符重载

--- Overload/main.cpp
+++ Overload/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 
@@ -45,9 +47,29 @@ public:
     }
 };
 
-//int operator+(const Student& a, const Student& b) {return 2*a.age+b.age;}
-int main()
-{
+class Hero {
+public:
+    int level;
+    int grade;
+    int exp;
+    string name;
+
+    Hero(string a,int b,int c,int d):name(a),level(b),grade(c),exp(d){}
+
+    //重载<  vector的sort中会用到///,比较
+    bool operator<(const Hero& other) const{
+        if(this->level != other.level) return this->level > other.level;
+        else if(this->grade != other.grade) return this->grade > other.grade;
+        else if(this->exp != other.exp) return this->exp > other.exp;
+    }
+};
+
+ostream& operator<<(ostream& os, const Hero& a)  {
+    os << "Name:" << a.name;
+    return os;
+}
+
+int main() {
     Person p1(10, 10);
     Person p2(20, 20);
     Person p3 = p1 + p2; // 使用重载的加法运算符
@@ -62,6 +84,24 @@ int main()
     Student f(2, "xujingning");
 
     cout<< ++f << f++ << f.age<<endl;
+
+    Hero a("demonhunter",3,3,3);
+    Hero b("guner",1,4,2);
+    Hero c("cow",1,5,3);
+    vector<Hero> vec;
+    vec.push_back(c);
+    vec.push_back(b);
+    vec.push_back(a);
+
+    sort(vec.begin(),vec.end());
+
+    auto itr=vec.begin();
+
+    for(;itr!=vec.end();++itr) {
+        cout << itr->name <<endl;
+    }
+
+    cout<<a<<endl;
 
     return 0;
 }
